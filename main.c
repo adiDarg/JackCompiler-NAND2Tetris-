0a9b213bcb648b05245764dacf12123fcc117f5e
Adi Dargatsky <adidargatsky144@gmail.com>
2026-01-08 12:31:51 +0200

    commit for random changes

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include "JackAnalyzer/JackAnalyzer.h"
 
 int main(void) {
-    operateOnFile("../data/input/ArrayTest/Main.jack","../data/output/intermediate.xml");
+    operateOnFile("../data/input/Average/Main.jack","../data/output/intermediate.xml");
     return 0;
 }

8eb6089d4123552707bdf9f1e8179b2a443bbb5a
Adi Dargatsky <adidargatsky144@gmail.com>
2026-04-25 11:53:29 +0300

    Added standard library classes and functions to tables

--- SemanticAnalysis/ClassTable/ClassTable.c
+++ SemanticAnalysis/ClassTable/ClassTable.c
@@ -4,26 +4,34 @@
 
 #include "ClassTable.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "../HashingFunctions/HashingFunctions.h"
+void addStandardLibClasses(const ClassTable *self);
 ClassTable* class_table_constructor(const size_t tableSize) {
     ClassTable *table = malloc(sizeof(ClassTable));
     table->size = tableSize;
     table->classes = malloc(sizeof(ClassList*)*tableSize);
+    for (int i = 0; i < tableSize; i++) {
+        table->classes[i] = NULL;
+    }
+    addStandardLibClasses(table);
     return table;
 }
 char defineClass(const ClassTable *self,char *name, const int nameLength) {
     const int hash = fnv1a_hash(name,nameLength) % self->size;
     ClassList **listPtr = &self->classes[hash];
+
     while (*listPtr != NULL) {
         if (strcmp((*listPtr)->name,name) == 0) {
             return 0;
         }
-        *listPtr = (*listPtr)->next;
+        listPtr = &((*listPtr)->next);
     }
     *listPtr = malloc(sizeof(ClassList));
+    if (*listPtr == NULL) return 0;
     (*listPtr)->name = name;
     (*listPtr)->next = NULL;
     return 1;
@@ -38,4 +46,23 @@ char doesClassExist(const ClassTable *self,const char *name, const int nameLengt
         *listPtr = (*listPtr)->next;
     }
     return 0;
+}
+
+typedef struct {
+    char *name;
+    int len;
+} OSClass;
+#define CLASS_ENTRY(n) { n, sizeof(n) - 1 }
+
+void addStandardLibClasses(const ClassTable *self) {
+    static const OSClass classes[] = {
+        CLASS_ENTRY("Math"),    CLASS_ENTRY("String"),
+        CLASS_ENTRY("Array"),   CLASS_ENTRY("Output"),
+        CLASS_ENTRY("Screen"),  CLASS_ENTRY("Keyboard"),
+        CLASS_ENTRY("Memory"),  CLASS_ENTRY("Sys")
+    };
+
+    for (int i = 0; i < sizeof(classes) / sizeof(OSClass); i++) {
+        defineClass(self, classes[i].name, classes[i].len);
+    }
 }
--- SemanticAnalysis/RoutineTable/RoutineTable.c
+++ SemanticAnalysis/RoutineTable/RoutineTable.c
@@ -6,15 +6,19 @@
 
 #include <string.h>
 #include "../HashingFunctions/HashingFunctions.h"
+
+//TODO: add handling for function parameters
+void addStandardLibRoutines(const RoutineTable *self);
 RoutineTable* routine_table_constructor(const size_t tableSize) {
     RoutineTable* table = malloc(sizeof(RoutineTable));
     table->size = tableSize;
     table->routines = malloc(tableSize * sizeof(RoutineList*));
+    for (int i = 0; i < tableSize; i++) {
+        table->routines[i] = NULL;
+    }
+    addStandardLibRoutines(table);
     return table;
 }
-
-
-
 char defineRoutine(const RoutineTable *self,const RoutineKind kind, const char *name, const int nameLength,
     const char *type, const int typeLength, const char *class, const int classLength) {
     Routine *routine = malloc(sizeof(Routine));
@@ -31,7 +35,7 @@ char defineRoutine(const RoutineTable *self,const RoutineKind kind, const char *
         if (strcmp((*listPtr)->routine->name,name) == 0) {
             return 0;
         }
-        *listPtr = (*listPtr)->next;
+        listPtr = &((*listPtr)->next);
     }
     *listPtr = malloc(sizeof(RoutineList));
     (*listPtr)->routine = routine;
@@ -48,4 +52,87 @@ Routine* getRoutine(const RoutineTable *self,const char *name, const int nameLen
         list = list->next;
     }
     return NULL;
+}
+
+
+typedef struct {
+    RoutineKind kind;
+    char *name; int nameLen;
+    char *type; int typeLen;
+    char *class; int classLen;
+} OSroutine;
+#define ROUTINE_ENTRY(k,n,t,c) { k, n, sizeof(n) - 1, t, sizeof(t) - 1, c, sizeof(c) - 1 }
+void addStandardLibRoutines(const RoutineTable *self) {
+    static const OSroutine routines[] = {
+        //Math
+        ROUTINE_ENTRY(ROUTINE_FUNCTION,"init","void","Math"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION,"abs","int","Math"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION,"multiply","int","Math"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION,"divide","int","Math"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION,"min","int","Math"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION,"max","int","Math"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION,"sqrt","int","Math"),
+
+        //String
+        ROUTINE_ENTRY(ROUTINE_CONSTRUCTOR,"new","String","String"),
+        ROUTINE_ENTRY(ROUTINE_METHOD,"dispose","void","String"),
+        ROUTINE_ENTRY(ROUTINE_METHOD,"length","int","String"),
+        ROUTINE_ENTRY(ROUTINE_METHOD,"charAt","char","String"),
+        ROUTINE_ENTRY(ROUTINE_METHOD,"setCharAt","void","String"),
+        ROUTINE_ENTRY(ROUTINE_METHOD,"appendChar","String","String"),
+        ROUTINE_ENTRY(ROUTINE_METHOD,"eraseLastChar","void","String"),
+        ROUTINE_ENTRY(ROUTINE_METHOD,"intValue","int","String"),
+        ROUTINE_ENTRY(ROUTINE_METHOD,"setInt","void","String"),
+        ROUTINE_ENTRY(ROUTINE_METHOD,"backSpace","char","String"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION,"doubleQuote","char","String"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION,"newLine","char","String"),
+
+        //Array
+        ROUTINE_ENTRY(ROUTINE_CONSTRUCTOR, "new", "Array", "Array"),
+        ROUTINE_ENTRY(ROUTINE_METHOD, "dispose", "void", "Array"),
+
+        //Output
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "init", "void", "Output"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "moveCursor", "void", "Output"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "printChar", "void", "Output"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "printString", "void", "Output"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "printInt", "void", "Output"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "println", "void", "Output"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "backSpace", "void", "Output"),
+
+        //Screen
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "init", "void", "Screen"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "clearScreen", "void", "Screen"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "setColor", "void", "Screen"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "drawPixel", "void", "Screen"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "drawLine", "void", "Screen"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "drawRectangle", "void", "Screen"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "drawCircle", "void", "Screen"),
+
+        //Keyboard
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "init", "void", "Keyboard"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "keyPressed", "char", "Keyboard"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "readChar", "char", "Keyboard"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "readLine", "String", "Keyboard"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "readInt", "int", "Keyboard"),
+
+        //Memory
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "init", "void", "Memory"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "peek", "int", "Memory"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "poke", "void", "Memory"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "alloc", "Array", "Memory"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "deAlloc", "void", "Memory"),
+
+        //Sys
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "init", "void", "Sys"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "halt", "void", "Sys"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "error", "void", "Sys"),
+        ROUTINE_ENTRY(ROUTINE_FUNCTION, "wait", "void", "Sys"),
+    };
+    for (int i = 0; i < sizeof(routines) / sizeof(OSroutine); i++) {
+        defineRoutine(self,routines[i].kind,
+            routines[i].name,routines[i].nameLen,
+            routines[i].type,routines[i].typeLen,
+            routines[i].class,routines[i].classLen);
+    }
 }
